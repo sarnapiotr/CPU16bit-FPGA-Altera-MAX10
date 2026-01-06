44ddc21106db46794e22a20ef442994af0b478ca
Piotr Sarna <95252277+sarnapiotr@users.noreply.github.com>
2026-01-06 19:14:57 +0100

    NOP, WAIT, CALL, RET signals + Interruption handling

--- ControlUnit/ControlUnit.vhd
+++ ControlUnit/ControlUnit.vhd
@@ -10,12 +10,12 @@ port(
 	Sa : out std_logic_vector(1 downto 0);
 	Sid : out std_logic_vector(2 downto 0);
 	Salu, Sbb, Sbc, Sba : out std_logic_vector(4 downto 0);
-	LDF, Smar, Smbr, WR, RD, INTout : out std_logic;
+	LDF, Smar, Smbr, WR, RD, INTout : out std_logic
 );
 end ControlUnit;
 
 architecture rtl of ControlUnit is
-	type state_type is (m0, m1, m9, m10);
+	type state_type is (m0, m1, m10, m11, m12, m13m, m14, m15);
 	signal state : state_type;
 begin
 	process(clk, reset)
@@ -27,10 +27,28 @@ begin
 			case state is
 				when m0 =>
 					state <= m1;
+					
 				when m1 =>
 					case IR(15 downto 13) is
 						when "000" =>
-						
+							case IR(12 downto 11) is
+								when "00" =>
+									if INT = '0' then state <= m0;
+									else state <= m11;
+									end if;
+									
+								when "01" => state <= m10;		
+								
+								when "10" => state <= m11;
+								
+								when "11" => state <= m15;
+								
+								when others =>
+									if INT = '0' then state <= m0;
+									else state <= m11;
+									end if;
+							end case;
+							
 						when "001" =>
 						
 						when "010" =>
@@ -41,7 +59,32 @@ begin
 						
 						when "101" =>
 						
+						when others =>
+							if INT = '0' then state <= m0;
+							else state <= m11;
+							end if;
 					end case;
+				
+				when m10 =>
+					if INT = '1' then state <= m11;
+					else state <= m10;
+					end if;
+					
+				when m11 => state <= m12;
+				
+				when m12 => state <= m13;
+				
+				when m13 => state <= m14;
+				
+				when m14 =>
+					if INT = '0' then state <= m0;
+					else state <= m11;
+					end if;
+				
+				when others =>
+					if INT = '0' then state <= m0;
+					else state <= m11;
+					end if;
 			end case;
 		end if;
 	end process;
@@ -49,10 +92,35 @@ begin
 	process(state)
 	begin
 		case state is
-			when m0 =>
+			when m0 => -- Fetch
 				Sa <= "01"; Sid <= "001"; Sbb <= "00000"; Sbc <= "00000"; Sba <= "00000"; Salu <= "00000"; LDF <= '0';
 				Smar <= '1'; Smbr <= '0'; WR <= '0'; RD <= '1';  INTout <= '0';
-			when m1 =>
+				
+			when m1 => -- Decode
+				Sa <= "00"; Sid <= "000"; Sbb <= "00000"; Sbc <= "00000"; Sba <= "00000"; Salu <= "00000"; LDF <= '0';
+				Smar <= '0'; Smbr <= '0'; WR <= '0'; RD <= '0'; INTout <= '0';
+				
+			when m10 => -- Wait
+				Sa <= "00"; Sid <= "000"; Sbb <= "00000"; Sbc <= "00000"; Sba <= "00000"; Salu <= "00000"; LDF <= '0';
+				Smar <= '0'; Smbr <= '0'; WR <= '0'; RD <= '0'; INTout <= '0';
+				
+			when m11 => -- PCl to stack
+				Sa <= "10"; Sid <= "011"; Sbb <= "10101"; Sbc <= "00000"; Sba <= "00000"; Salu <= "00000"; LDF <= '0';
+				Smar <= '1'; Smbr <= '1'; WR <= '0'; RD <= '0'; INTout <= '1';
+				
+			when m12 => -- PCh to stack
+				Sa <= "10"; Sid <= "011"; Sbb <= "10100"; Sbc <= "00000"; Sba <= "00000"; Salu <= "00000"; LDF <= '0';
+				Smar <= '1'; Smbr <= '1'; WR <= '1'; RD <= '0'; INTout <= '1';
+				
+			when m13 => -- ATMPl to PCl
+				Sa <= "00"; Sid <= "000"; Sbb <= "11001"; Sbc <= "00000"; Sba <= "10101"; Salu <= "00000"; LDF <= '0';
+				Smar <= '0'; Smbr <= '0'; WR <= '1'; RD <= '0'; INTout <= '1';
+				
+			when m14 => -- ATMPh to PCh
+				Sa <= "00"; Sid <= "000"; Sbb <= "11000"; Sbc <= "00000"; Sba <= "10100"; Salu <= "00000"; LDF <= '0';
+				Smar <= '0'; Smbr <= '0'; WR <= '0'; RD <= '0'; INTout <= '1';
+			
+			when others =>
 				Sa <= "00"; Sid <= "000"; Sbb <= "00000"; Sbc <= "00000"; Sba <= "00000"; Salu <= "00000"; LDF <= '0';
 				Smar <= '0'; Smbr <= '0'; WR <= '0'; RD <= '0'; INTout <= '0';
 		end case;
